b408524ffd14483e346e0c95fff5c91f5dde2a73
Lumen-X <onohsomtochukwubasil@gmail.com>
2020-07-02 23:04:58 +0100

    fixed 232: Can now add store assistants

--- routes/user.js
+++ routes/user.js
@@ -4,25 +4,25 @@ const users = require("../controllers/user.controller.js");
 const bodyValidator = require("../util/body_validator");
 const auth = require("../auth/auth");
 
-router.use("/user", auth);
+router.use("/assistant", auth);
 //Add new user
 router.post(
-  "/store-assistant/new",
+  "/assistant/new",
   auth,
   users.validate("body"),
   bodyValidator,
   users.new
 );
 // Retrieve all Users
-router.get("/user/all/:current_user", auth, users.all);
+router.get("/assistant/all/:current_user", auth, users.all);
 
 // Retrieve a single User with user_id
-router.get("/user/:user_id", auth, users.getById);
+router.get("/assistant/:assistant_id", auth, users.getById);
 
 // Update User Info with user_id
-router.put("/user/update/:user_id", auth, users.update);
+router.put("/assistant/update/:assistant_id", auth, users.update);
 
 // Delete a User with user_id
-router.delete("/user/delete/:user_id", auth, users.delete);
+router.delete("/assistant/delete/:assistant_id", auth, users.delete);
 
 module.exports = router;
